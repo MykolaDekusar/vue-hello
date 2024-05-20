4b8d4d40d9c4b76dee48e257ab2df960bc33a44d
MykolaDekusar <ndekusar97@gmail.com>
2024-05-20 15:17:27 +0200

    Experimented with mounted()

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,14 @@ createApp({
       message: "Hello Vue!",
       src: "https://picsum.photos/200",
       alt: "Random Image",
+      calculate: 2 + 2,
     };
+  },
+  mounted() {
+    console.log(this.message);
+    if (this.calculate > 4) {
+      this.message = "Bye Vue!";
+      this.calculate++;
+    }
   },
 }).mount("#app");
